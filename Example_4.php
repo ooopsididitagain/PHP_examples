087e81a6a9752d43fc87eeb08e116480d74a1ee9
ooopsididitagain <gleboff.iv@yandex.ru>
2021-01-22 18:30:47 +0300

    Update Example_4.php

--- Example_4.php
+++ Example_4.php
@@ -8,6 +8,11 @@
 function max_and_min_value_change($first_start_number = 1, $first_end_number = 100, $second_start_number = 1,
                                   $second_end_number = 100)
 {
+    if (!is_int($first_start_number) or !is_int($first_end_number) or !is_int($second_start_number)
+        or !is_int($second_end_number)) {
+        return 'Вводите только целые числа.';
+
+    }
     $random_array = range(rand($first_start_number, $first_end_number), rand($second_start_number, $second_end_number));
     shuffle($random_array);
     $max_value = max($random_array);
