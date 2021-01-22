be3f408274c8fd660c3f78ab530aaf91fa02c1c7
ooopsididitagain <gleboff.iv@yandex.ru>
2021-01-22 17:39:47 +0300

    Edit Example_1.php and Example_2.php

--- Example_2.php
+++ Example_2.php
@@ -1,19 +1,17 @@
 <?php
 /*
-Вам нужно разработать программу, которая считала бы количество вхождений какой-нибуть выбранной вами цифры
-в выбранном вами числе. Например: цифра 5 в числе 442158755745 встречается 4 раза
+ * Вам нужно разработать программу, которая считала бы количество вхождений какой-нибуть выбранной вами цифры
+ * в выбранном вами числе. Например: цифра 5 в числе 442158755745 встречается 4 раза
 */
-
-
 
 
 function count_of_number($search_number = 1, $number = 111)
 {
 
     if (is_int($number) and is_int($search_number)) {
-        return count(array_filter(str_split($number), function($k) use ($search_number){
-            return $k == $search_number;
-        },)) . '<br/>';
+        return count(array_filter(str_split($number), function ($k) use ($search_number) {
+                return $k == $search_number;
+            },)) . '<br/>';
 
     } else {
         return 'Введите целое число. <br/>';
